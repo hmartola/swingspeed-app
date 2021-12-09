6ea2e6d9fb5a04eac844831fcbb500c498c0252a
hmartola <64286599+hmartola@users.noreply.github.com>
2021-12-09 22:32:12 +0200

    add root functionality

--- src/App.js
+++ src/App.js
@@ -10,20 +10,57 @@
 // 2. npx react-native run-android --no-jetifier
 
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { View, Text } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import MainMenu from './navigation/mainMenu';
+import LoginScreen from './screens/login';
+import CreateAccountScreen from './screens/createAccount';
+//import { getLogin } from './components/loginContext'
 
 const App = () => {
-    return (
+
+    const [userLoggedIn, setUserLoggedIn] = useState(false)
+
+    useEffect(() => {
+        (async () =>  {
+            const value = await AsyncStorage.getItem('user')
+            if (value !== null) {
+                setUserLoggedIn(true)
+                console.log(value, 'via useffect')
+            } else {
+                setUserLoggedIn(false)
+                console.log(value, 'via useffect')
+            } 
+        })()
+    })
     
+    const Stack = createNativeStackNavigator()
+    //console.log('Logged in? ' + getLogin())
+
+    if (userLoggedIn) {
+        console.log('user is logged in')
+        return (
+            <NavigationContainer>
+                    <Stack.Navigator screenOptions={{ headerShown: false }}>
+                        <Stack.Screen name='Main Menu' component={MainMenu}></Stack.Screen>
+                        <Stack.Screen name='Login' component={LoginScreen}></Stack.Screen>
+                        <Stack.Screen name='CreateAccount' component={CreateAccountScreen}></Stack.Screen> 
+                    </Stack.Navigator>
+                </NavigationContainer>
+        )
+    }
+    return (
         <NavigationContainer>
-            <MainMenu />
-        </NavigationContainer>
-
-
+                <Stack.Navigator screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name='Login' component={LoginScreen}></Stack.Screen>
+                    <Stack.Screen name='CreateAccount' component={CreateAccountScreen}></Stack.Screen>
+                    <Stack.Screen name='Main Menu' component={MainMenu}></Stack.Screen>
+                </Stack.Navigator>
+            </NavigationContainer>
     )
+    
 }
 
 
