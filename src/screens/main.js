a3c8b7c4d8cd0410a88d16361c35874598206c73
hmartola <64286599+hmartola@users.noreply.github.com>
2022-02-07 01:50:42 +0200

    refactor scren UI and functionality

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,26 +1,41 @@
-import React from "react"
-import { Button, Text, View } from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from "react"
+import { Text, View, useWindowDimensions } from 'react-native'
+import { Button } from "react-native-elements";
 
 import { getUser } from '../components/user'
 
 const MainScreen = ({ navigation }) => {
-    
+
+    const [username, setUsername] = useState('')
+
+    const { height, width } = useWindowDimensions()
+
+    useEffect(() => {
+       (async () => {
+         const name = await getUser()
+         setUsername(name)  
+       })() 
+    }, [])
 
     return (
-        <View style={{ flex: 1, justifyContent: 'flex-start' }}>
-            <Text style={{ padding: 15 }}>
-                <Text style={{ fontSize: 40, color: 'darkgreen' }}>Velofore</Text>
+        <View style={{ flex: 1, justifyContent: 'center', display: 'flex', marginLeft: width*.025, marginRight: width*.025, marginTop: height*.01 }}>
+   
+            <Text style={{ fontSize: 40 }}>
+                <Text style={{ color: '#008811' }}>Welcome, </Text>
+                <Text style={{ color: '#434343' }}>{username}</Text>
             </Text>
 
-            <Text style={{ fontSize: 20, marginTop: 40, textAlign: 'center' }}>Start a new session?</Text>
-            
-            <View style={{ alignItems: 'center', padding: 15 }}>
-                <Button onPress={() => navigation.navigate('Camera')} // open camera
-                        title='start swinging' 
-                        color='darkgreen'>
-                </Button>
-            </View>
+            <Text style={{ fontSize: 25, marginTop: 50, textAlign: 'center', color: '#2c2c2c' }}>Start a new session?</Text>
+            <Button 
+                title='OPEN CAMERA' 
+                type='outline' 
+                raised
+                buttonStyle={{ backgroundColor: '#008811' }}
+                titleStyle={{ color: 'white' }}
+                containerStyle={{ width: '45%', alignSelf: 'center', marginTop: 15 }}
+                onPress={() => navigation.navigate('Camera')}
+            />
+        
         </View>
     )
 }
