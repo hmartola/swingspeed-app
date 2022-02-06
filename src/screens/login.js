ec1434446d48e340f2331e7e6461b24b0823c51c
hmartola <64286599+hmartola@users.noreply.github.com>
2022-02-07 01:49:48 +0200

    refactor screen UI and functionality

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,26 +1,31 @@
 import React, { useContext, useState } from "react"
-import { TextInput, View, Text, Image, TouchableOpacity, useWindowDimensions } from "react-native"
-import { Button } from "react-native-elements"
+import { View, Text, useWindowDimensions } from "react-native"
+import { Button, Input } from "react-native-elements"
 
 import handleLogin from "../components/handleLogin"
 import { LoginContext } from "../contexts/loginContext"
+import Velofore from '../assets/Velofore.svg'
 
 const LoginScreen = ({ navigation }) => {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
 
     const { isLoggedIn, setIsLoggedIn } = useContext(LoginContext)
     const { height, width } = useWindowDimensions()
 
     const onLogin = async () => {
         try {
+            setIsLoading(true)
             const login = await handleLogin(username, password)
             setPassword('')
             
             if (login) {
+                setIsLoading(false)
                 setIsLoggedIn(true)
             } else {
+                setIsLoading(false)
                 setIsLoggedIn(false)
             }
 
@@ -28,40 +33,33 @@ const LoginScreen = ({ navigation }) => {
             console.log(e) 
           }
     }
-
+    
     return (
-        <View style={{ flex: 1, justifyContent: 'center', display: 'flex', marginLeft: width*.05, marginRight: width*.05 }}>
-            
-            <Text style={{ padding: 15, fontSize: 30, color: 'darkgreen', alignSelf: 'center' }}>Choose user to sign in</Text>
-            
-            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                <TouchableOpacity onPress={() => onLogin()}>
-                    <Image source={require('../assets/Robert.jpg')} style={{ width: 100, height: 100, borderRadius: 100 }} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => onLogin()} style={{ backgroundColor: 'lightgray', borderRadius: 100, width: 100, justifyContent: 'center' }}>
-                    <Text style={{ textAlign: 'center', fontWeight: 'bold', color: 'darkgreen' }}>GUEST</Text>
-                </TouchableOpacity>
-            </View>
+        <View style={{ flex: 1, justifyContent: 'center', display: 'flex', marginLeft: width*.05, marginRight: width*.05, marginTop: height*.01 }}>
+            <Velofore width={300} height={100} style={{ alignSelf: 'center', marginRight: width*.4 }} />
             
             <View style={{ alignItems: 'center' }}>
-                <Text style={{ padding: 15, marginTop: 30, fontSize: 20, color: 'darkgreen', alignSelf: 'center' }}>Or sign in with different account</Text>
-                <TextInput 
+                <Text style={{ padding: 15, marginTop: 30, fontSize: 20, color: 'darkgreen', alignSelf: 'center' }}>Sign in with your account</Text>
+                <Input 
                     placeholder='Enter username or email' 
-                    onChangeText={username => setUsername(username)} defaultValue={username}
-                    style={{ borderBottomWidth: 1, width: '95%', marginBottom: 10 }} />
-                <TextInput 
+                    onChangeText={username => setUsername(username)} 
+                    defaultValue={username}
+                />
+                <Input 
                     placeholder='Enter password' 
-                    onChangeText={password => setPassword(password)} defaultValue={password}
+                    onChangeText={password => setPassword(password)} 
+                    defaultValue={password}
                     secureTextEntry={true}
-                    style={{ borderBottomWidth: 1, width: '95%', marginBottom: 10 }} />
+                />
             </View>
 
-            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 15 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 15, alignItems: 'center' }}>
                 <Button 
                     title='Login'  
                     type='outline' 
                     raised
-                    buttonStyle={{ backgroundColor: 'darkgreen' }}
+                    loading={isLoading}
+                    buttonStyle={{ backgroundColor: '#00bd26' }}
                     titleStyle={{ color: 'white' }}
                     containerStyle={{ width: '45%'}}  
                     onPress={() => onLogin()}
@@ -70,7 +68,7 @@ const LoginScreen = ({ navigation }) => {
                     title='Forgot password?'  
                     type='clear' 
                     titleStyle={{ color: 'blue' }}
-                    containerStyle={{ width: '45%'}}  
+                    containerStyle={{ width: '45%' }}  
                     onPress={() => console.log('pressed forgot password')}
                 />
             </View>
@@ -81,7 +79,7 @@ const LoginScreen = ({ navigation }) => {
                     title='Create account' 
                     type='outline' 
                     raised
-                    buttonStyle={{ backgroundColor: 'darkgreen' }}
+                    buttonStyle={{ backgroundColor: '#00bd26' }}
                     titleStyle={{ color: 'white' }}
                     containerStyle={{ width: '45%'}}
                     onPress={() => navigation.navigate('CreateAccount')} 
