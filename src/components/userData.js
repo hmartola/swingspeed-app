dc080fc33b458f24f5b3c76a236c5024ac46e3fb
hmartola <64286599+hmartola@users.noreply.github.com>
2022-03-24 00:51:46 +0200

    add user data to profile

--- src/components/userData.js
+++ src/components/userData.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { View } from "react-native"
 import { ListItem } from "react-native-elements"
+
+import userService from '../service/users'
+import { storeUserData, getUserData } from "./user"
 
 const UserData = ({ username }) => {
 
@@ -10,9 +13,27 @@ const UserData = ({ username }) => {
 	const [isLefty, setIsLefty] = useState(false)
 	const [isMetric, setIsMetric] = useState(true)
 	//const [name, setName] = useState(username)  // TODO: ABLE TO ALSO ASSIGN REAL NAME TO ACCOUNT ?
-	const [age, setAge] = useState('')
-	const [height, setHeight] = useState('')
-	const [hcp, setHcp] = useState('')
+	const [age, setAge] = useState('Type your age')
+	const [height, setHeight] = useState('Type your height')
+	const [hcp, setHcp] = useState('Type your HCP')
+	const [topSpeed, setTopSpeed] = useState('No recorded data')
+
+	useEffect(() => {
+		(async () => {
+			const userData = await getUserData()
+			if (userData !== null) {
+				console.log('local data', userData)
+				setData(userData)
+			} 
+			else {
+				const userObj = await userService.getUserData()
+				console.log('api data', userObj)
+				if (userObj !== null) {
+					setData(userObj)
+				}
+			}
+		})()
+	}, [])
 
 	const data = {
 		age: age,
@@ -20,12 +41,40 @@ const UserData = ({ username }) => {
 		height: height,
 		lefty: isLefty,
 		units: isMetric,
-		hcp: hcp
+		handicap: hcp
 	}
 
-	const handleSubmit = () => {
-		console.log(data)
+	const setData = (userObj) => {
+		console.log(userObj)
+		if (userObj.age !== null) {
+			setAge(userObj.age)
+		}
+		if (userObj.height !== null) {
+			setHeight(userObj.height)
+		}
+		if (userObj.handicap !== null) {
+			setHcp(userObj.handicap)
+		}
+		if (userObj.lefty) {
+			handleHandedness(0)
+		}
+		if (userObj.top_speed !== null) {
+			setTopSpeed(userObj.top_speed)
+		}
+		if (userObj.gender !== null) {
+			setSelectedGenderButtonIndex(userObj.gender)
+		}
 	}
+
+	const handleSubmit = async () => {
+		await storeUserData(data)
+		console.log('submit', data)
+	}
+
+/* 	const handleGender = (index) => {
+		setSelectedGenderButtonIndex(index)
+		handleSubmit()
+	} */
 
 	const handleHandedness = (index) => {
 		setSelectedHandednessButtonIndex(index)
@@ -51,18 +100,21 @@ const UserData = ({ username }) => {
 		<View style={{ marginTop: 15 }}>
 			<ListItem bottomDivider>
 				<ListItem.Content>
-					<ListItem.Title>Name</ListItem.Title>
+					<ListItem.Title>Username</ListItem.Title>
 				</ListItem.Content>
-				<ListItem.Title>{username}</ListItem.Title>
+				<ListItem.Title style={{ width: 180, textAlign: 'right' }}>{username}</ListItem.Title>
 			</ListItem>
 			<ListItem bottomDivider>
 				<ListItem.Content>
 					<ListItem.Title>Age</ListItem.Title>
 				</ListItem.Content>
 				<ListItem.Input
-					placeholder='Enter your age'
+					defaultValue={age.toString()}
+					keyboardType='number-pad'
 					onChangeText={age => setAge(age)}
-					onBlur={() => handleSubmit()} />
+					onFocus={() => setAge('')}
+					onBlur={() => handleSubmit()} 
+					containerStyle={{ textAlign: 'right', paddingHorizontal: 0 }}/>
 			</ListItem>
 			<ListItem bottomDivider>
 				<ListItem.Content>
@@ -71,18 +123,21 @@ const UserData = ({ username }) => {
 				<ListItem.ButtonGroup
 					buttons={['Male', 'Female', 'Other']}
 					selectedIndex={selectedGenderButtonIndex}
-					onPress={(index) => setSelectedGenderButtonIndex(index)}
+					onPress={(index) => { setSelectedGenderButtonIndex(index), handleSubmit() }}
 					selectedButtonStyle={{ backgroundColor: '#008811' }}
 				/>
 			</ListItem>
 			<ListItem bottomDivider>
 				<ListItem.Content>
-					<ListItem.Title>Height</ListItem.Title>
+					<ListItem.Title>Height (cm)</ListItem.Title>
 				</ListItem.Content>
 				<ListItem.Input
-					placeholder='Enter your height'
+					defaultValue={height.toString()}
+					keyboardType='number-pad'
 					onChangeText={height => setHeight(height)}
-					onBlur={() => handleSubmit()} />
+					onFocus={() => setHeight('')}
+					onBlur={() => handleSubmit()} 
+					containerStyle={{ textAlign: 'right', paddingHorizontal: 0 }}/>
 			</ListItem>
 			<ListItem bottomDivider>
 				<ListItem.Content>
@@ -111,15 +166,18 @@ const UserData = ({ username }) => {
 					<ListItem.Title>HCP</ListItem.Title>
 				</ListItem.Content>
 				<ListItem.Input
-					placeholder='Enter your HCP'
+					defaultValue={hcp.toString()}
+					keyboardType='number-pad'
 					onChangeText={hcp => setHcp(hcp)}
-					onBlur={() => handleSubmit()} />
+					onFocus={() => setHcp('')}
+					onBlur={() => handleSubmit()} 
+					containerStyle={{ textAlign: 'right', paddingHorizontal: 0 }}/>
 			</ListItem>
 			<ListItem bottomDivider>
 				<ListItem.Content>
 					<ListItem.Title>Top swing speed</ListItem.Title>
 				</ListItem.Content>
-				<ListItem.Title>No recorded data</ListItem.Title>
+				<ListItem.Title>{topSpeed}</ListItem.Title>
 			</ListItem>
 		</View>
 
