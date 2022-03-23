d92c920becdd0aa1a45a9174719b1aed5d098af1
hmartola <64286599+hmartola@users.noreply.github.com>
2022-03-24 00:53:46 +0200

    refactor main page UI

--- src/screens/main.js
+++ src/screens/main.js
@@ -18,23 +18,30 @@ const MainScreen = ({ navigation }) => {
 	}, [])
 
 	return (
-		<View style={{ flex: 1, justifyContent: 'center', display: 'flex', marginLeft: width * .025, marginRight: width * .025, marginTop: height * .01 }}>
+		<View style={{ flex: 1, justifyContent: 'flex-start', display: 'flex', marginLeft: width * .025, marginRight: width * .025, marginTop: height * .01 }}>
 
+		<View style={{ marginTop: 30 }}>
 			<Text style={{ fontSize: 40 }}>
-				<Text style={{ color: '#008811' }}>Welcome, </Text>
-				<Text style={{ color: '#434343' }}>{username}</Text>
+				<Text style={{ color: '#008811' }}>Hello </Text>
+				<Text style={{ color: '#434343' }}>{username} &#x1F44B;</Text>
 			</Text>
+		</View>
 
-			<Text style={{ fontSize: 25, marginTop: 50, textAlign: 'center', color: '#2c2c2c' }}>Start a new session?</Text>
+		<View style={{ borderWidth: 1, marginTop: 50, padding: 20 }}>
+			<Text style={{ fontSize: 22, textAlign: 'center', color: '#2c2c2c' }}>
+				Start a new session?
+			</Text>
+			
 			<Button
 				title='OPEN CAMERA'
 				type='outline'
 				raised
-				buttonStyle={{ backgroundColor: '#008811' }}
+				buttonStyle={{ backgroundColor: '#008811', borderRadius: 3 }}
 				titleStyle={{ color: 'white' }}
 				containerStyle={{ width: '45%', alignSelf: 'center', marginTop: 15 }}
 				onPress={() => navigation.navigate('Camera')}
 			/>
+		</View>
 
 		</View>
 	)
