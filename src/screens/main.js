cb663455aced8b44f063025e9593861be6bc22f7
hmartola <64286599+hmartola@users.noreply.github.com>
2022-03-25 01:10:31 +0200

    add latest and top swing

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,38 +1,71 @@
 import React, { useEffect, useState } from "react"
 import { Text, View, useWindowDimensions } from 'react-native'
-import { Button } from "react-native-elements";
+import { Button, Card } from "react-native-elements";
+import { useIsFocused } from "@react-navigation/native";
 
 import { getUser } from '../components/user'
+import userService from '../service/users'
 
 const MainScreen = ({ navigation }) => {
 
 	const [username, setUsername] = useState('')
+	const [swingData, setSwingData] = useState([])
 
 	const { height, width } = useWindowDimensions()
+	const isFocused = useIsFocused()
 
 	useEffect(() => {
 		(async () => {
 			const name = await getUser()
 			setUsername(name)
+			const swings = await userService.getUserSwings()
+			setSwingData(swings)
 		})()
-	}, [])
+	}, [isFocused])
+
+	const getLatestSwing = () => {
+		try {
+			const latestSwing = swingData.slice(-1)
+			return latestSwing[0].speed
+		} catch (err) {
+			console.warn(err)
+		}
+	}
+
+	const getTopSwing = () => {
+		try {
+			const swings = swingData.map(swings => {
+				return swings.speed
+			})
+			const topSwing = Math.max(...swings)
+			return topSwing
+		} catch (err) {
+			console.warn(err)
+		}
+	}
 
 	return (
-		<View style={{ flex: 1, justifyContent: 'flex-start', display: 'flex', marginLeft: width * .025, marginRight: width * .025, marginTop: height * .01 }}>
+		<View 
+			style={{ 
+				flex: 1, 
+				justifyContent: 'flex-start', 
+				display: 'flex', 
+				marginLeft: width * .025, 
+				marginRight: width * .025, 
+				marginTop: height * .01 
+			}}>
 
 		<View style={{ marginTop: 30 }}>
-			<Text style={{ fontSize: 40 }}>
+			<Text style={{ fontSize: 40, textAlign: 'center' }}>
 				<Text style={{ color: '#008811' }}>Hello </Text>
 				<Text style={{ color: '#434343' }}>{username} &#x1F44B;</Text>
 			</Text>
 		</View>
 
-		<View style={{ borderWidth: 1, marginTop: 50, padding: 20 }}>
-			<Text style={{ fontSize: 22, textAlign: 'center', color: '#2c2c2c' }}>
-				Start a new session?
-			</Text>
-			
-			<Button
+		<View style={{ marginTop: 50 }}>
+			<Card>
+				<Card.Title style={{ fontSize: 25 }}>Start a new session?</Card.Title>
+				<Button
 				title='OPEN CAMERA'
 				type='outline'
 				raised
@@ -41,6 +74,31 @@ const MainScreen = ({ navigation }) => {
 				containerStyle={{ width: '45%', alignSelf: 'center', marginTop: 15 }}
 				onPress={() => navigation.navigate('Camera')}
 			/>
+			</Card>
+		</View>
+
+		<View style={{ marginTop: 5 }}>
+			<Card>
+				<Card.Title style={{ fontSize: 20 }}>Your latest swing speed</Card.Title>
+				<Card.Divider />
+				{swingData.length !== 0 ? (
+					<Text style={{ textAlign: 'center', color: '#434343' }}>{getLatestSwing()} kph</Text>
+				) : (
+					<Text style={{ textAlign: 'center', color: '#434343' }}>0 kph</Text>
+				)}
+			</Card>
+		</View>
+
+		<View style={{ marginTop: 5 }}>
+			<Card>
+				<Card.Title style={{ fontSize: 20 }}>Your top swing speed</Card.Title>
+				<Card.Divider />
+				{swingData.length !== 0 ? (
+					<Text style={{ textAlign: 'center', color: '#434343' }}>{getTopSwing()} kph</Text>
+				) : (
+					<Text style={{ textAlign: 'center', color: '#434343' }}>0 kph</Text>
+				)}
+			</Card>
 		</View>
 
 		</View>
